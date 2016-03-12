2b2d51047cfa87412b9717d9f67c26246cf63e64
tejus <tejus.111@gmail.com>
2016-03-12 15:43:39 +0000

    Initial commit of web app, with a .gitignore file

---
+++ flask_app.py
@@ -0,0 +1,67 @@
+# all the imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+
+
+
+# create our little application :)
+app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.secret_key = 'tihishastobeaC!0n%^$@<>;;;}}}:)'
+
+print "i am here"
+@app.route('/')
+def index():
+ print "now here"
+ if not session.get('logged_in'):
+    return redirect(url_for('login'))
+ else:
+    return redirect(url_for('welcome'))
+
+@app.route('/welcome', methods=['GET', 'POST'])
+def welcome():
+ if not session.get('logged_in'):
+    return redirect(url_for('index'))
+ else:
+    return render_template('welcome.html',title='Our Magic',page_title='Welcome ' + session.get('username').title(),username=session.get('username'))
+
+@app.route('/musings', methods=['GET', 'POST'])
+def musings():
+ if not session.get('logged_in'):
+    return redirect(url_for('index'))
+ else:
+    return render_template('musings.html',title='Our Magic',page_title='Thanks ' + session.get('username').title(),username=session.get('username'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    print "yeah error is none"
+    print request.method
+    if request.method == 'POST':
+        print request.form['inputUsername']
+        print request.form['inputPassword']
+        if request.form['inputUsername'] != 'tejus':
+            error = 'Invalid username'
+            print "the username was invalid"
+        elif request.form['inputPassword'] != 'remember':
+            error = 'Invalid password'
+        else:
+
+            session['logged_in'] = True
+            session['username'] = request.form['inputUsername']
+            print "just before login"
+            return redirect(url_for('index'))
+    return render_template('login.html',
+                         title='LogIn - Its specialized for you',
+						 page_title='Our Magic',error=error)
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
