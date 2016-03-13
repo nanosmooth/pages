a80729fc19f2cc8836950edefc4dd1b76f2e7124
Tejus <tejus.111@gmail.com>
2016-03-13 12:25:07 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -42,7 +42,9 @@ def musings():
  if not session.get('logged_in'):
     return redirect(url_for('index'))
  else:
-    return render_template('musings.html',title='Our Magic',page_title='Our Magic'.title(),username=session.get('username'),comments=Comment.query.all())
+    if request.method == "GET":
+    	return render_template('musings.html', page_title='Our Magic'.title(),username=session.get('username'),comments=Comment.query.all())
+    return render_template('musings.html',title='Our Magic',page_title='Our Magic'.title(),username=session.get('username'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
